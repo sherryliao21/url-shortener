677fbaf84a8408f1c6e8543b174908346c959c57
Sherry <sherry.c.liao@gmail.com>
2021-02-05 22:20:37 +0800

    feat: add mongoose connection

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/url-shortener', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
