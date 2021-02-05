cebcbddb9ad604cb41f8bfbb620347ce357d6954
Sherry <sherry.c.liao@gmail.com>
2021-02-05 23:03:24 +0800

    feat: add results page

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/url-shortener', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
+const bodyParser = require('body-parser')
+const URL = require('./models/urls')
 
 db.on('error', () => {
   console.log('mongodb error!')
@@ -16,9 +18,28 @@ db.once('open', () => {
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/result', (req, res) => {
+  res.render('result')
+})
+
+app.post('/', (req, res) => {
+  const shortenedURL = generateShortenedURL()
+  const targetURL = req.body.link
+  console.log(req.body)
+  console.log(shortenedURL)
+  function generateShortenedURL() {
+    let shortURL = '87jwB'
+    return shortURL
+  }
+  return URL.create({ targetURL, shortenedURL })
+    .then(() => res.redirect('result'))
+    .catch(error => console.log(error))
 })
 
 app.listen(PORT, () => {
