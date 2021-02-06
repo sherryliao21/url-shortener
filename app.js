bf4b7abd5f824b17349fb5df74ee82da797dd2b0
Sherry <sherry.c.liao@gmail.com>
2021-02-06 18:53:47 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const URL = require('./models/urls')
 const routes = require('./routes')
 require('./config/mongoose')
-
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,6 @@
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/url-shortener'
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/url-shortener', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 
 db.on('error', () => {
